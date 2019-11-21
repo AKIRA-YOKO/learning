ff94c6eb6b097c827eed9dfd9278731dbe33d424
souunn <yokota123aki@gmail.com>
2019-11-21 11:08:21 +0900

    python hello world
    my first commit

---
+++ program3_5.py
@@ -0,0 +1,20 @@
+print('1 + 3は？')
+print(1 + 3)
+
+print('10 - 3は？')
+print(10 - 3)
+
+print('3 * 3 * 33は？')
+print(3 * 3 * 33)
+
+print('164 / 3は？')
+print(164 / 3)
+
+print('164 / 3の余りは？')
+print(164 % 3)
+
+print('164 // 3は？')
+print(164 // 3)
+
+print('3 + 3 * 33は？')
+print(3 + 3 * 33)
